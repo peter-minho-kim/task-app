47e8d6dbcd05020261761693e367751b700d3169
Peter Kim <peter.minho.kim@gmail.com>
2020-10-31 16:00:22 -0400

    Adds image upload api

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const multer = require('multer');
 const User = require('../models/user');
 const auth = require('../middleware/auth');
 
 const router = new express.Router();
+
+const upload = multer({ dest: 'avatars' });
 
 router.post('/users', async (req, res) => {
   const user = new User(req.body);
@@ -14,6 +17,10 @@ router.post('/users', async (req, res) => {
   } catch (e) {
     res.status(400).send(e);
   }
+});
+
+router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+  res.send();
 });
 
 router.post('/users/login', async (req, res) => {
